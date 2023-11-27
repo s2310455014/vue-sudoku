d8da06be86f7b9fd4ec2292c6f7ead8cfb1fbc43
AzureAD\ElenaEbetshuber <elena.ebetshuber@paso-solutions.com>
2023-11-27 10:27:21 +0100

    apply his changes

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="board">
+    <h1>Sudoku</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/utils/isValidSudokuNumber.ts
@@ -0,0 +1,4 @@
+export const isValidSudokuNumber = (value: number): boolean => {
+  if (!Number.isInteger(value)) return false
+  return value >= 1 && value <= 9
+}
